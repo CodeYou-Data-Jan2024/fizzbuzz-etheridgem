5da53c79f393fd43da65fee917ff1cbe7b44c263
Morgan Etheridge <154018020+etheridgem@users.noreply.github.com>
2024-04-02 18:40:44 -0400

    Revert "Feedback"

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,2 @@
-for fizzbuzz in range(1, 101):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-        print("FizzBuzz")
-    elif fizzbuzz % 3 == 0:
-        print("Fizz")
-    elif fizzbuzz % 5 == 0:
-        print("Buzz")
-    else:
-        print(fizzbuzz)
+# add your code here
 
